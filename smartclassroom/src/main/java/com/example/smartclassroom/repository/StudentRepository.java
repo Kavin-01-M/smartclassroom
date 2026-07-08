972072dd6450def9e3d232b96ff488d4ba2ed5f7
Kavin Manivel <kavinmanivel81@gmail.com>
2026-07-08 13:34:11 +0530

    Initial commit

---
+++ smartclassroom/src/main/java/com/example/smartclassroom/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.smartclassroom.repository;
+
+import com.example.smartclassroom.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
